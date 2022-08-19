3e3b84d469e36ba298981c374f65988e75c3bb48
mint25mt <tihiro.haul@gmail.com>
2022-08-19 15:55:12 +0900

    armでビルドできてない？

--- tcpproxy_server.cpp
+++ tcpproxy_server.cpp
@@ -8,8 +8,30 @@
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread/mutex.hpp>
+#include <boost/thread.hpp>
 
 #include "server.h++"
+
+void run_proxy(unsigned short local_port, unsigned short forward_port
+    , const std::string local_host, const std::string forward_host)
+{
+
+    boost::asio::io_service ios;
+    try
+    {
+        tcp_proxy::bridge::acceptor acceptor(ios,
+                                             local_host, local_port,
+                                             forward_host, forward_port);
+
+        acceptor.accept_connections();
+
+        ios.run();
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -19,28 +41,18 @@ int main(int argc, char* argv[])
       return 1;
    }
 
+   const int num_threads = 4;
    const auto local_port   = static_cast<unsigned short>(::atoi(argv[2]));
    const auto forward_port = static_cast<unsigned short>(::atoi(argv[4]));
    const std::string local_host      = argv[1];
    const std::string forward_host    = argv[3];
 
-   boost::asio::io_service ios;
+    boost::thread_group threads;
+    for (int i = 0; i < num_threads; i++)
+        threads.create_thread(boost::bind(run_proxy, local_port, forward_port, local_host, forward_host));
 
-   try
-   {
-      tcp_proxy::bridge::acceptor acceptor(ios,
-                                           local_host, local_port,
-                                           forward_host, forward_port);
 
-      acceptor.accept_connections();
-
-      ios.run();
-   }
-   catch(std::exception& e)
-   {
-      std::cerr << "Error: " << e.what() << std::endl;
-      return 1;
-   }
+    threads.join_all();
 
    return 0;
 }
