6da0844aeb7a052b6515e016fe7c3f9b94bc8fe5
Wyutaka <tihiro.haul@gmail.com>
2022-10-06 02:28:01 +0000

    gcc17を明示的に指定

---
+++ build/CMakeFiles/tcpproxy_server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpproxy_server.dir/acceptor.cpp.o"
+  "CMakeFiles/tcpproxy_server.dir/acceptor.cpp.o.d"
+  "CMakeFiles/tcpproxy_server.dir/bridge.cpp.o"
+  "CMakeFiles/tcpproxy_server.dir/bridge.cpp.o.d"
+  "CMakeFiles/tcpproxy_server.dir/tcpproxy_server.cpp.o"
+  "CMakeFiles/tcpproxy_server.dir/tcpproxy_server.cpp.o.d"
+  "bin/tcpproxy_server"
+  "bin/tcpproxy_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpproxy_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
