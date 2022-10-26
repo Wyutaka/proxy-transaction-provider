78db3dd7aee7cd6ce3f687c692f7049d7e447210
Wyutaka <tihiro.haul@gmail.com>
2022-10-26 16:37:37 +0900

    musi

---
+++ build/CMakeFiles/tcpproxy_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpproxy_server.dir/acceptor.cpp.o"
+  "CMakeFiles/tcpproxy_server.dir/bridge.cpp.o"
+  "CMakeFiles/tcpproxy_server.dir/tcpproxy_server.cpp.o"
+  "bin/tcpproxy_server"
+  "bin/tcpproxy_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpproxy_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
